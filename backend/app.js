2e8f4da10e49cdf209a7b2caac153de018ee5b7c
Kartikeya <kartikeya7.96@gmail.com>
2025-04-22 20:14:01 +0530

    Added vercel config and gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const cors=require('cors');
-const run = require('./utils/db.connect');
+const express = require("express");
+const cors = require("cors");
+const run = require("./utils/db.connect");
 
-const app=express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.get('/projects',async(req,res)=>{
-  const data=await run().catch(console.dir); 
-  res.json(data)
-})
+app.get("/projects", async (req, res) => {
+  const data = await run().catch(console.dir);
+  res.json(data);
+});
 
-app.listen(8081,()=>{
-    console.log('server started on 8081')
-})
+app.listen(process.env.PORT || 8081, () => {
+  console.log("server started on 8081");
+});
