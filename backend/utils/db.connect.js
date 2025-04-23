47fd1c3d24ec559975bc6eefdd00f7f3a3a3a4ed
Kartikeya <kartikeya7.96@gmail.com>
2025-04-23 11:52:14 +0530

    updated env variables logic for deployment

--- backend/utils/db.connect.js
+++ backend/utils/db.connect.js
@@ -1,5 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const uri = process.env.DB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -7,21 +9,25 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      const data=await client.db("portfolio-api").collection("projects").find().toArray();
-      return data
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const data = await client
+      .db("portfolio-api")
+      .collection("projects")
+      .find()
+      .toArray();
+    return data;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
+}
 
-module.exports=run
+module.exports = run;
